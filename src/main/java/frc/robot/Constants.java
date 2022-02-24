872f8ddf7e5fd319ec3493b922fb82c195ca88c2
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2022-02-23 16:59:32 -0800

    Moved RobotMap.java and OI.java into Constants.java
    Moved constants into Constants.java from old RobotMap.java and OI.java (except for Joystick Access method) to comply with new template (and as shown in Practice Project 2021).  Thanks to @spacejunkl (CBS) for helpings.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,24 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+/**
+ * From RobotMap.java ((c) 2019)
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into to a variable name.
+ * This provides flexibility changing wiring, makes checking the wiring easier and significantly
+ * reduces the number of magic numbers floating around.
+ */
+public final class Constants {
+  // Moved from old RobotMap.java and OI.java (except for Joystick Access method to comply with new template (and as shown in Practice Project 2021).  Thanks to @spacejunkl (CBS) for helpings.
+  // Controls
+  public static final int CONTROLLER_PORT_ID = 0; // Joystick
+
+  // Left joystick
+  public static final int JOYSTICK_DRIVE_SPEED_ID = 1; // Y-axis
+  public static final int JOYSTICK_DRIVE_ROTATION_ID = 0; // X-axis
+
+  // Drivetrain
+  public static final int MOTOR_LEFT_FRONT_ID = 10;
+  public static final int MOTOR_LEFT_BACK_ID = 11;
+  public static final int MOTOR_RIGHT_FRONT_ID = 12;
+  public static final int MOTOR_RIGHT_BACK_ID = 13;
+}
