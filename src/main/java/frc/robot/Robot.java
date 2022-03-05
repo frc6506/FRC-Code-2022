fa7b34435602e3c2307d2edd2b860aac4e0005f8
BobSaidHi <47067448+BobSaidHi@users.noreply.github.com>
2022-03-05 10:57:05 -0800

    Fixed bug were drivetrain was created twice, leading to error with duplicated SparkMax can IDs
    I guess the initiation goes in robot container and not robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Drivetrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -20,9 +19,6 @@ public class Robot extends TimedRobot {
   //public static OI m_oi;
 
   private RobotContainer m_robotContainer;
-
-  // subsystems
-  public static Drivetrain drivetrain = new Drivetrain();
 
   /**
    * This function is run when the robot is first started up and should be used for any
